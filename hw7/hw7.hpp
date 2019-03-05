42c536b63efb0ee02f88798c0b11ce809b7f7625
Austin-Rhonehouse <46973274+Austin-Rhonehouse@users.noreply.github.com>
2019-03-04 23:32:33 -0500

    Add files via upload

---
+++ hw7/hw7.hpp
@@ -0,0 +1,15 @@
+
+#ifndef hw7_hpp
+#define hw7_hpp
+
+#include <stdio.h>
+#include <stdio.h>
+#include <iostream>
+#include <cstdarg>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+double stddev(int n, ...);
+
+#endif  /*hw7_hpp*/
